6f1bb8243b3249bbd9547e6896166808134a3097
Kanishk Mittal <kanishkmittal26@gmail.com>
2023-07-17 18:50:23 +0530

    All react

--- 05. jQuery/02_Jquery.js
+++ 05. jQuery/02_Jquery.js
@@ -15,29 +15,27 @@ $("h1").addClass("big-title");
 
 
 // manipulating text using jQuery
-
 $("h1").text("bye");
-
-
 $("button").html("<em>Hey</em>")
 
 
 // manipulating attributes of elements using jQuery
 
-$("a").attr("href","https://www.yahoo.com");
+$("a").attr("href", "https://www.w3docs.com");
 
-var nameOfSite=console.log($("a").text());
+// var nameOfSite=console.log($("a").text());
 
-nameOfSite="yahoo"; 
+// nameOfSite="w3Schools"; 
 
-$("a").text(nameOfSite);
+// $("a").text(nameOfSite);
 
 
 // adding eventListeners using jQuery
 
 // 1.
-$("h1").click(function(){
-        $("h1").text("my name is Kanishk").addClass("kanishk").css("background-color","violet");
+$("h1").click(function()
+{
+    $("h1").text("my name is Kanishk").addClass("kanishk").css("background-color","violet");
 });
 
 
@@ -48,7 +46,6 @@ $("h1").click(function(){
 // for(var i=0;i<5;i++){
 //     document.querySelectorAll("button")[i].addEventListener("click",function(){
 //         document.querySelector("h1").style.color="purple";
-
 //         setTimeout(function(){
 //             document.querySelector("h1").style.color="yellow";
 //         },1000);
@@ -68,67 +65,67 @@ $("button").click(function(){
 
 
 
-$("input").keypress(function(event){
-    console.log(event.key);
-});
-
-
-
-// task
-// when u press anything on body the text inside h1 should change as the value you are pressing simultaneously
-
-
-
-// $(document).keypress(function(event){
-//     $("h1").text(event.key);
+// $("input").keypress(function(event){
+//     console.log(event.key);
 // });
+
+
+
+// // task
+// // when u press anything on body the text inside h1 should change as the value you are pressing simultaneously
+
+
+
+// // $(document).keypress(function(event){
+// //     $("h1").text(event.key);
+// // });
  
 
-$("h1").on("mouseover",function(){
-    $("h1").text("google");
+// $("h1").on("mouseover",function(){
+//     $("h1").text("google");
 
-    setTimeout(function(){
-        $("h1").text("Kanishk");
-    },1000);
+//     setTimeout(function(){
+//         $("h1").text("Kanishk");
+//     },1000);
 
-});
-
-
-// adding elements using jQuery
-
-$("h1").before("<button>button before</button>");
-{/* <button>button before</button> */}
-{/* <h1></h1> */}
-
-$("h1").after("<button>button before</button>");
-{/* <h1></h1> */}
-{/* <button>button before</button> */}
+// });
 
 
-$("h1").prepend("<button>button before</button>");
-{/* <h1><button>button before</button>Hello</h1> */}
+// // adding elements using jQuery
+
+// $("h1").before("<button>button before</button>");
+// {/* <button>button before</button> */}
+// {/* <h1></h1> */}
+
+// $("h1").after("<button>button before</button>");
+// {/* <h1></h1> */}
+// {/* <button>button before</button> */}
 
 
-$("h1").append("<button>button before</button>");
-{/* <h1>Hello<button>button before</button></h1> */}
+// $("h1").prepend("<button>button before</button>");
+// {/* <h1><button>button before</button>Hello</h1> */}
 
 
-// adding animations using jQuery
-
-$("button").on("click",function(){
-    $("h1").slideUp().slideDown();
-});
+// $("h1").append("<button>button before</button>");
+// {/* <h1>Hello<button>button before</button></h1> */}
 
 
-//    or
+// // adding animations using jQuery
 
-$("button").on("click",function(){
-    $("h1").slideUp().slideDown().animate({opacity: 0.5});
-});
+// $("button").on("click",function(){
+//     $("h1").slideUp().slideDown();
+// });
 
 
-//    or
+// //    or
 
-$("button").on("click",function(){
-    $("h1").hide();
-});
+// $("button").on("click",function(){
+//     $("h1").slideUp().slideDown().animate({opacity: 0.5});
+// });
+
+
+// //    or
+
+// $("button").on("click",function(){
+//     $("h1").hide();
+// });
